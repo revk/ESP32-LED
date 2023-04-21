76a54ec6ab2c0c2ca376833e707bbd770208b6f4
RevK <github@k.gg>
2023-04-21 18:56:16 +0100

    start

--- main/stargate.c
+++ main/stargate.c
@@ -5,17 +5,18 @@
 const char *
 stargate (app_t * a)
 {
+      uint8_t top;
+   int8_t dir = 1;
+   if (a->top < 0)
+   {
+      top = (leds + a->start + a->top) % leds;
+      dir = -1;
+   } else
+      top = (leds + a->top - a->start) % leds;
    if (!a->cycle)
    {                            // Sanity checks, etc
 	   if(!a->limit)a->limit=60*cps;
    }
-   uint8_t top;
-   int8_t dir = 1;
-   if (a->top < 0)
-   {
-      top = a->start - a->top;
-      dir = -1;
-   } else
-      top = a->top - a->start;
+
    return "No stargate yet";
 }
