815c9268509248b4ade2c1b82481111b415fa443
RevK <github@k.gg>
2023-04-21 15:39:15 +0100

    Idle app

--- main/idle.c
+++ main/idle.c
@@ -2,8 +2,15 @@
 
 #include "app.h"
 
-void
+const char *
 idle (app_t * a)
 {
-
+   uint8_t l = 255;
+   if (a->fade && a->cycle < a->fade)
+      l = 255 * (a->cycle + 1) / a->fade;
+   else if (a->limit && a->limit - a->cycle < a->fade)
+      l = 255 * (a->limit - a->cycle + 1) / a->fade;
+   for (int i = 0; i < a->len; i++)
+      setl (i, a, l);
+   return NULL;
 }
