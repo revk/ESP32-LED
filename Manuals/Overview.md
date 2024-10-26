66e8c8e452184700d11c1ca1fb61a151ccc6edc0
RevK <github@k.gg>
2024-10-26 09:19:55 +0100

    volumergb config

--- Manuals/Overview.md
+++ Manuals/Overview.md
@@ -112,7 +112,9 @@ A palette is simply a way colours change, they can be based on time, or position
 |---|-------|
 |`audio`|Spectrum based display. Note, on RGBW, if W is not set, it is overridden with an over peak output white|
 |`volume`|Volume bar graph.|
-|`volumergb`|Volume RGB bar graph (red:high, green:mid, blue:base). Note, setting a colour causes colours to be restricted to colour set, e.g. set blue for *base*|
+|`volumergb`|Volume RGB bar graph (red:high, green:mid, blue:base). Note, setting a *colour* causes colours to be restricted to colour set, e.g. set blue for *base*. Cannot use with a palette|
+
+Note that `volumergb` uses blue for *base* and red for *high* by default, but a JSON config in a preset can set `"r"`/`"g"`/`"b"` as an array of *start* and *len* in the 24 frequency bins instead.
 
 ### Ring/loop apps
 
--- main/volumergb.c
+++ main/volumergb.c
@@ -46,40 +46,100 @@ pixelb (app_t * a, int pos, int p, int n, uint8_t l)
    setB (pos + a->start, (int) a->b * c[pos] / 255);
 }
 
+typedef struct
+{
+   uint8_t start;
+   uint8_t len;
+} band_t;
+
+typedef struct
+{
+   band_t r,
+     g,
+     b;
+} config_t;
+
 const char *
 appvolumergb (app_t * a)
 {
    if (!a->cycle)
    {                            // Sanity check / defaults
       free (a->data);
-      memset (a->data = malloc (a->len * 3), 0, a->len * 3);
+      memset (a->data = malloc (a->len * 3 + sizeof (config_t)), 0, a->len * 3 + sizeof (config_t));
       if (!a->colourset || a->palette)
          setcolour (a, "white");
    }
-
+   config_t *c = a->data + a->len * 3;
+   if (!a->cycle)
+   {
+      if (a->preset && *config[a->preset - 1])
+      {
+         jo_t j = jo_parse_str (config[a->preset - 1]);
+         if (jo_here (j) == JO_OBJECT)
+         {
+            while (jo_next (j) == JO_TAG)
+            {
+               band_t *b = NULL;
+               if (!jo_strcmp (j, "r"))
+                  b = &c->r;
+               else if (!jo_strcmp (j, "g"))
+                  b = &c->g;
+               else if (!jo_strcmp (j, "b"))
+                  b = &c->b;
+               if (jo_next (j) != JO_ARRAY || !b)
+               {
+                  jo_skip (j);
+                  continue;
+               }
+               if (jo_next (j) == JO_NUMBER)
+               {
+                  b->start = jo_read_int (j);
+                  if (jo_next (j) == JO_NUMBER)
+                     b->len = jo_read_int (j);
+                  else
+                     b->len = AUDIOBANDS / 3;
+               }
+               while (jo_here (j) && jo_here (j) != JO_CLOSE)
+                  jo_next (j);
+            }
+         }
+         jo_free (&j);
+      } else
+      {                         // No config - set defaults
+         c->b.start = 0;
+         c->b.len = AUDIOBANDS / 4;
+         c->g.start = AUDIOBANDS / 4;
+         c->g.len = AUDIOBANDS / 4;
+         c->r.start = AUDIOBANDS / 2;
+         c->r.len = AUDIOBANDS / 2;
+      }
+   }
+   if (c->b.len)
    {                            // Low 1/4
       float m = 0;
-      for (int i = 0 * AUDIOBANDS / 4; i < 1 * AUDIOBANDS / 4; i++)
+      for (uint8_t i = c->b.start; i < c->b.start + c->b.len && i < AUDIOBANDS; i++)
          m += audioband[i];
-      int v = m * 65536 / (AUDIOBANDS / 4);
+      int v = m * 65536 / c->b.len;
       if (v > 65535)
          v = 65535;
       bargraph (a, pixelb, v, 65535, a->fader);
    }
+   if (c->g.len)
    {                            // Mid 1/4
       float m = 0;
-      for (int i = 1 * AUDIOBANDS / 4; i < 2 * AUDIOBANDS / 4; i++)
+      for (uint8_t i = c->g.start; i < c->g.start + c->g.len && i < AUDIOBANDS; i++)
          m += audioband[i];
-      int v = m * 65535 / (AUDIOBANDS / 4);
+      int v = m * 65535 / c->g.len;
       if (v > 65535)
          v = 65535;
       bargraph (a, pixelg, v, 65535, a->fader);
    }
+   if (c->r.len)
    {                            // High 1/2
       float m = 0;
-      for (int i = 2 * AUDIOBANDS / 4; i < 4 * AUDIOBANDS / 4; i++)
+      for (uint8_t i = c->r.start; i < c->r.start + c->r.len && i < AUDIOBANDS; i++)
          m += audioband[i];
-      int v = m * 65535 / (AUDIOBANDS / 2);
+      int v = m * 65535 / c->r.len;
       if (v > 65535)
          v = 65535;
       bargraph (a, pixelr, v, 65535, a->fader);
